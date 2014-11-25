fced9270381384a330ab3f42fdf26ef174f15517
Toshinori Sato <overlasting@gmail.com>
2014-11-25 17:47:52 +0900

    Add check process of installed libraries

--- sh/make_centos_env.sh
+++ sh/make_centos_env.sh
@@ -1,62 +1,78 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(cd $(dirname $0);pwd)
-SCRIPT_NAME="[make_centos_env]"
+SCRIPT_NAME="[make_centos_env] :"
 TMP_DIR=/tmp/msgpack_rpc_c
+
+echo "$SCRIPT_NAME Get sudo password"
+sudo pwd
 
 mkdir $TMP_DIR
 cd $TMP_DIR
 
-echo "$SCRIPT_NAME trying to install msgpack-c.."
-git clone https://github.com/msgpack/msgpack-c.git
-cd msgpack-c
-./bootstrap
-./configure CXXFLAGS="-std=c++11"
-make
-sudo make install
+if [ -e /usr/local/lib/libmsgpack.so.4.0.0 ]; then
+    echo "$SCRIPT_NAME msgpack-c is already installed.."
+else
+    echo "$SCRIPT_NAME trying to install msgpack-c.."
+    git clone https://github.com/msgpack/msgpack-c.git
+    cd msgpack-c
+    git checkout refs/tags/cpp-0.5.9
+    ./bootstrap
+    ./configure
+    make
+    sudo make install
 
-echo "$SCRIPT_NAME making clean msgpack-c directory.."
-cd $TMP_DIR
-rm -rf msgpack-c
+    echo "$SCRIPT_NAME making clean msgpack-c directory.."
+    cd $TMP_DIR
+    rm -rf msgpack-c
 
-echo "$SCRIPT_NAME trying to install mpio.."
-git clone https://github.com/frsyuki/mpio.git
-cd mpio
-./bootstrap
-./configure
-make
-sudo make install
+    echo "$SCRIPT_NAME Refreshing the cache of shared library.."
+    sudo ldconfig
+fi
 
-echo "$SCRIPT_NAME making clean mpio directory.."
-cd $TMP_DIR
-rm -rf mpio
+if [ -e /usr/local/lib/libmpio.so.0.0.0 ]; then
+    echo "$SCRIPT_NAME mpio is already installed.."
+else
+    echo "$SCRIPT_NAME trying to install mpio.."
+    git clone https://github.com/frsyuki/mpio.git
+    cd mpio
+    ./bootstrap
+    ./configure
+    make
+    sudo make install
 
-echo "$SCRIPT_NAME Refreshing the cache of shared library.."
-sudo ldconfig
+    echo "$SCRIPT_NAME making clean mpio directory.."
+    cd $TMP_DIR
+    rm -rf mpio
 
-echo "$SCRIPT_NAME trying to install msgpack-rpc-cpp.."
-git clone https://github.com/msgpack-rpc/msgpack-rpc-cpp.git
-cd msgpack-rpc-cpp
-./bootstrap  # if needed
-./configure
-make
-sudo make install
+    echo "$SCRIPT_NAME Refreshing the cache of shared library.."
+    sudo ldconfig
+fi
 
-echo "$SCRIPT_NAME Refreshing the cache of shared library.."
-sudo ldconfig
+if [ -e /usr/local/lib/libmsgpack-rpc.so.1.0.0 ]; then
+    echo "$SCRIPT_NAME msgpack-rpc-cpp is already installed.."
+else
+    echo "$SCRIPT_NAME trying to install msgpack-rpc-cpp.."
+    git clone https://github.com/msgpack-rpc/msgpack-rpc-cpp.git
+    cd msgpack-rpc-cpp
+    ./bootstrap  # if needed
+    ./configure
+    make
+    sudo make install
 
-echo "$SCRIPT_NAME making clean msgpack-rpc-cpp directory.."
-cd $TMP_DIR
-rm -rf msgpack-rpc-cpp
+    echo "$SCRIPT_NAME making clean msgpack-rpc-cpp directory.."
+    cd $TMP_DIR
+    rm -rf msgpack-rpc-cpp
+
+    echo "$SCRIPT_NAME Refreshing the cache of shared library.."
+    sudo ldconfig
+fi
 
 echo "$SCRIPT_NAME Making clean a build directory.."
-cd $BASEDIR
+ncd $BASEDIR
 rm -rf $TMP_DIR
 
 echo "$SCRIPT_NAME Trying to install waf.."
 sudo yum install -y waf
-
-echo "$SCRIPT_NAME Trying to install jansson.."
-sudo yum install -y jansson
 
 echo "$SCRIPT_NAME Finish.."
